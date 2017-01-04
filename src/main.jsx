11a69ba5ad73bd0343ec6f85c08ff5e8d4f4bbfa
Nicholas Ko <mailnickko@gmail.com>
2017-01-03 19:29:21 -0800

    (feat) Add connection to redux dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 // From App
 import './styles/styles.scss';
 import rootReducer from './reducers';
 import routes from './routes';
 
+const enhancers = compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+)
 const routingMiddleware = routerMiddleware(browserHistory);
-const store = createStore(rootReducer, applyMiddleware(thunk, routingMiddleware));
+const store = createStore(rootReducer, enhancers, applyMiddleware(thunk, routingMiddleware));
 const history = syncHistoryWithStore(browserHistory, store);
 
 const router = (
